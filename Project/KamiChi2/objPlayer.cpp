7b42b5d57627d15389b50109916975e91f77c86f
Antony Wells <antonyrobwells@gmail.com>
2019-11-08 17:42:17 +0000

    progress

--- Project/KamiChi2/objPlayer.cpp
+++ Project/KamiChi2/objPlayer.cpp
@@ -16,7 +16,28 @@ objPlayer::objPlayer() {
 	adown->setFrame(new kImage("data/img/obj/ship/player/down2-game.png"), 1);
 	adown->setFrame(new kImage("data/img/obj/ship/player/down3-game.png"), 2);
 	adown->setFrame(new kImage("data/img/obj/ship/player/down4-game.png"), 3);
+
+	aup = new kAnimSet(4);
+
+	aup->setFrame(new  kImage("data/img/obj/ship/player/up1-game.png"), 0);
+	aup->setFrame(new kImage("data/img/obj/ship/player/up2-game.png"), 1);
+	aup->setFrame(new kImage("data/img/obj/ship/player/up3-game.png"), 2);
+	aup->setFrame(new kImage("data/img/obj/ship/player/up4-game.png"), 3);
 	
+	aright = new kAnimSet(4);
+
+	aright->setFrame(new kImage("data/img/obj/ship/player/right1-game.png"), 0);
+	aright->setFrame(new kImage("data/img/obj/ship/player/right2-game.png"), 1);
+	aright->setFrame(new kImage("data/img/obj/ship/player/right3-game.png"), 2);
+	aright->setFrame(new kImage("data/img/obj/ship/player/right4-game.png"), 3);
+	 
+	aleft = new kAnimSet(4);
+
+	aleft->setFrame(new kImage("data/img/obj/ship/player/left1-game.png"), 0);
+	aleft->setFrame(new kImage("data/img/obj/ship/player/left2-game.png"), 1);
+	aleft->setFrame(new kImage("data/img/obj/ship/player/left3-game.png"), 2);
+	aleft->setFrame(new kImage("data/img/obj/ship/player/left4-game.png"), 3);
+
 	setAnim(aidle, 0.1f);
 	setSize(182, 108);
 	
--- Project/KamiChi2/objPlayer.h
+++ Project/KamiChi2/objPlayer.h
@@ -9,10 +9,45 @@ public:
 	void update();
 	void render();
 	void init();
+	
+	void left() {
+
+		if (getCurAnim() == aidle || getCurAnim() == aup || getCurAnim() == adown) {
+
+			aleft->setType(ANIMTYPE::Once);
+			setAnim(aleft, 0.4);
+
+		}
+
+	}
+
+	void right() {
+
+		if (getCurAnim() == aidle || getCurAnim() == aup || getCurAnim() == adown)
+		{
+
+			aright->setType(ANIMTYPE::Once);
+			setAnim(aright, 0.4);
+
+		}
+
+
+	}
+
+	void up() {
+
+		if (getCurAnim() == aidle || getCurAnim() == adown) {
+
+			aup->setType(ANIMTYPE::Once);
+			setAnim(aup, 0.4f);
+
+		}
+
+	}
 
 	void down() {
 
-		if (getCurAnim() == aidle) {
+		if (getCurAnim() == aidle || getCurAnim() == aup) {
 
 			adown->setType(ANIMTYPE::Once);
 			setAnim(adown, 0.4f);
@@ -25,6 +60,76 @@ public:
 
 		if (getCurAnim() != aidle) {
 
+			if (getCurAnim() == aleft) {
+
+				if (getCurAnim()->getAnimType() == ANIMTYPE::Once)
+				{
+
+					aleft->setType(ANIMTYPE::BackOnce);
+					aleft->setSpeed(0.4f);
+					aleft->reset();
+
+				}
+				else if (getCurAnim()->getAnimType() == ANIMTYPE::BackOnce)
+				{
+					//exit(1);
+					if (getCurAnim()->isDone()) {
+
+						setAnim(aidle, 0);
+
+					}
+
+				}
+
+
+			}
+
+			if (getCurAnim() == aright) {
+
+				if (getCurAnim()->getAnimType() == ANIMTYPE::Once)
+				{
+
+					aright->setType(ANIMTYPE::BackOnce);
+					aright->setSpeed(0.4f);
+					aright->reset();
+
+				}
+				else if (getCurAnim()->getAnimType() == ANIMTYPE::BackOnce)
+				{
+					//exit(1);
+					if (getCurAnim()->isDone()) {
+
+						setAnim(aidle, 0);
+
+					}
+
+				}
+
+
+			}
+
+			if (getCurAnim() == aup) {
+
+				if (getCurAnim()->getAnimType() == ANIMTYPE::Once)
+				{
+
+					aup->setType(ANIMTYPE::BackOnce);
+					aup->setSpeed(0.4f);
+					aup->reset();
+
+				}
+				else if (getCurAnim()->getAnimType() == ANIMTYPE::BackOnce)
+				{
+					//exit(1);
+					if (getCurAnim()->isDone()) {
+
+						setAnim(aidle, 0);
+
+					}
+
+				}
+
+			}
 			if (getCurAnim() == adown) {
 
 				if (getCurAnim()->getAnimType() == ANIMTYPE::Once)
@@ -59,6 +164,8 @@ private:
 	kAnimSet* aidle;
 	kAnimSet* aup;
 	kAnimSet* adown;
+	kAnimSet* aright;
+	kAnimSet* aleft;
 
 };
 
